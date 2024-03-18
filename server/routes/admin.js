06e3e9c3bc61cdfdfda6096d35b829d38c2300bc
Marco Veio - Diadema <marcoveiocodes@google.com>
2024-03-18 20:50:17 +0100

    "admin layout, header, footer and dashboard"

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,5 +1,23 @@
 import express from 'express';
 const router = express.Router();
 
+// Layouts
+const adminLayout = '../views/layouts/admin-layout.ejs';
+
+// GET - Admin page
+router.get('/', async (req, res) => {
+    try {
+        const locals = {
+            title: "Admin",
+            description: "Simple blog created with NodeJs"
+        }        
+        res.render("admin/admin-index.ejs", { 
+            locals,
+            layout: adminLayout
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 export default router;
