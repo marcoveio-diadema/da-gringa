e5ded50da63b0924bc7e7f31d985761f1cb6ed4c
Marco Veio - Diadema <marcoveiocodes@google.com>
2024-03-19 09:59:43 +0100

    blog category, search routes and templates

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -12,4 +12,26 @@ router.get('/post', (req, res) => {
      });
 });
 
+// GET - Category page
+router.get('/category', (req, res) => {
+    const locals = {
+        title: 'Categoria',
+        description: "Tudo sobre como se virar na gringa!"
+    }
+    res.render('blog/category.ejs', { 
+        locals,
+     });
+});
+
+// GET - serch page
+router.get('/search', (req, res) => {
+    const locals = {
+        title: 'Busca',
+        description: "Tudo sobre como se virar na gringa!"
+    }
+    res.render('blog/search.ejs', { 
+        locals,
+     });
+});
+
 export default router;
