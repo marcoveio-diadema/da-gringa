2b1cb18c309b5135fd574e134ad173eee6ef0d54
Marco Veio - Diadema <marcoveiocodes@google.com>
2024-04-12 13:56:40 +0200

    sitemap.xml

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ import flash from 'express-flash';
 import moment from 'moment';
 import connectPgSimple from 'connect-pg-simple';
 import db from './server/config/db.js';
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { createGzip } from 'zlib';
 
 // import routes
 import mainRoutes from './server/routes/main.js';
@@ -63,6 +65,57 @@ app.use((req, res, next) => {
 
 // flash messages
 app.use(flash());
+
+
+// sitemap
+let sitemap;
+
+app.get('/sitemap.xml', async (req, res) => {
+    res.setHeader('Content-Type', 'application/xml');
+    res.setHeader('Content-Encoding', 'gzip');
+    
+    // if we have a cached entry send it
+    if (sitemap) {
+      res.send(sitemap);
+      return;
+    }
+  
+    try {
+      const smStream = new SitemapStream({ hostname: 'https://manual-da-gringa-tcx8.onrender.com/' });
+      const pipeline = smStream.pipe(createGzip());
+  
+      // pipe your entries or directly write them.
+      smStream.write({ url: '/contact/',  changefreq: 'daily', priority: 0.3 });
+      smStream.write({ url: '/about/',  changefreq: 'monthly', priority: 0.7 });
+
+       // Fetch posts from the database
+        const result = await db.query(`
+            SELECT posts.*, categories.category AS category_name, users.username AS author_username 
+            FROM posts
+            INNER JOIN categories ON posts.category_id = categories.id 
+            INNER JOIN users ON posts.author_id = users.id 
+            ORDER BY COALESCE(posts.updated_at, posts.created_at) DESC
+        `);
+
+        const posts = result.rows;
+
+        // Write each post's URL to the sitemap stream
+        for (const post of posts) {
+        smStream.write({ url: `/blog/post/${post.slug}/`,  changefreq: 'daily', priority: 0.8 });
+        }
+  
+      // cache the response
+      streamToPromise(pipeline).then(sm => sitemap = sm);
+  
+      // end sitemap stream
+      smStream.end();
+      // stream write the response
+      pipeline.pipe(res).on('error', (e) => {throw e;});
+    } catch (e) {
+      console.error(e);
+      res.status(500).end();
+    }
+  });
 
 // routes
 app.use('/', mainRoutes);
