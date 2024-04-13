414c83cc7d386ad9dccac6129a931c4e84146d60
Marco Veio - Diadema <marcoveiocodes@google.com>
2024-04-13 14:43:48 +0200

    sitemap and robots

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ const PORT = process.env.PORT;
 // static
 app.use(express.static('public'));
 
-
 // moment.js middleware
 app.use((req, res, next) => {
     res.locals.moment = moment;
@@ -42,7 +41,8 @@ const pgSession = connectPgSimple(session);
 // Passport.js and session middleware
 app.use(session({
     store: new pgSession({
-        conString: process.env.DB_STRING
+        conString: process.env.NODE_ENV === 'production' ? process.env.DB_STRING : 
+        `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?sslmode=disable`
     }),
     secret: process.env.SESSION_SECRET, 
     resave: false, 
@@ -66,63 +66,68 @@ app.use((req, res, next) => {
 // flash messages
 app.use(flash());
 
-
-// sitemap
-let sitemap;
-
-app.get('/sitemap.xml', async (req, res) => {
-    res.setHeader('Content-Type', 'application/xml');
-    res.setHeader('Content-Encoding', 'gzip');
-    
-    // if we have a cached entry send it
-    if (sitemap) {
-      res.send(sitemap);
-      return;
-    }
-  
-    try {
-      const smStream = new SitemapStream({ hostname: 'https://manual-da-gringa-tcx8.onrender.com/' });
-      const pipeline = smStream.pipe(createGzip());
-  
-      // pipe your entries or directly write them.
-      smStream.write({ url: '/contact/',  changefreq: 'daily', priority: 0.3 });
-      smStream.write({ url: '/about/',  changefreq: 'monthly', priority: 0.7 });
-
-       // Fetch posts from the database
-        const result = await db.query(`
-            SELECT posts.*, categories.category AS category_name, users.username AS author_username 
-            FROM posts
-            INNER JOIN categories ON posts.category_id = categories.id 
-            INNER JOIN users ON posts.author_id = users.id 
-            ORDER BY COALESCE(posts.updated_at, posts.created_at) DESC
-        `);
-
-        const posts = result.rows;
-
-        // Write each post's URL to the sitemap stream
-        for (const post of posts) {
-        smStream.write({ url: `/blog/post/${post.slug}/`,  changefreq: 'daily', priority: 0.8 });
-        }
-  
-      // cache the response
-      await streamToPromise(pipeline).then(sm => sitemap = sm);
-  
-      // end sitemap stream
-      smStream.end();
-      // stream write the response
-      pipeline.pipe(res).on('error', (e) => {throw e;});
-    } catch (e) {
-      console.error(e);
-      res.status(500).end();
-    }
-  });
-
 // routes
 app.use('/', mainRoutes);
 app.use('/user', userRoutes);
 app.use('/blog', blogRoutes);
 app.use('/admin', adminRoutes);
 
+// robots.txt
+app.get('/robots.txt', function (req, res) {
+    res.type('text/plain');
+    const hostname = process.env.NODE_ENV === 'production' ? process.env.HOSTNAME_PROD : process.env.HOSTNAME_DEV;
+    res.send(`User-agent: *\nAllow: /\nDisallow: /admin/\nSitemap: ${hostname}sitemap.xml`);
+});
+
+// sitemap
+let sitemap
+
+app.get('/sitemap.xml', async function(req, res) {
+  res.header('Content-Type', 'application/xml');
+  res.header('Content-Encoding', 'gzip');
+  // if we have a cached entry send it
+  if (sitemap) {
+    res.send(sitemap)
+    return
+  }
+
+  try {
+    const hostname = process.env.NODE_ENV === 'production' ? process.env.HOSTNAME_PROD : process.env.HOSTNAME_DEV;
+    const smStream = new SitemapStream({ hostname })
+    const pipeline = smStream.pipe(createGzip())
+
+    // pipe your entries or directly write them.
+    smStream.write({ url: '/',  changefreq: 'daily', priority: 0.9 })
+    smStream.write({ url: '/contact/',  changefreq: 'monthly',  priority: 0.7 })
+    smStream.write({ url: '/about/', changefreq: 'monthly', priority: 0.5})
+
+    // Fetch posts from the database
+    const result = await db.query(`
+        SELECT posts.*, categories.category AS category_name, users.username AS author_username 
+        FROM posts
+        INNER JOIN categories ON posts.category_id = categories.id 
+        INNER JOIN users ON posts.author_id = users.id 
+        ORDER BY COALESCE(posts.updated_at, posts.created_at) DESC
+    `);
+
+    const posts = result.rows;
+
+    // Add each post to the sitemap
+    posts.forEach(post => {
+      smStream.write({ url: `/post/${post.slug.trim()}/`, changefreq: 'weekly', priority: 0.8 })
+    });
+
+    // cache the response
+    streamToPromise(pipeline).then(sm => sitemap = sm)
+    // make sure to attach a write stream such as streamToPromise before ending
+    smStream.end()
+    // stream write the response
+    pipeline.pipe(res).on('error', (e) => {throw e})
+  } catch (e) {
+    console.error(e)
+    res.status(500).end()
+  }
+})
 
 // 500 route
 app.use((err, req, res, next) => {
--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,11 @@
 import pg from "pg";
 
-// create DB
+// Create DB
 const db = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
+  connectionString: process.env.NODE_ENV === 'production' ? process.env.DB_STRING : 
+  `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+});
+
 
 export default db
